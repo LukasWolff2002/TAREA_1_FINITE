4ed341aafdcbcb70d62f49a3c7a359ac81e172d6
Vicencio F. Felipe <favicencio@miuandes.cl>
2025-03-28 14:14:32 -0300

    mom

--- P1/main.py
+++ P1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from solver import Solver
 from matrix_assembly import Assembly
 from structure_assembly import Structure
-from graph import plot_original_structure_all_forces, plot_deformed_structure, plot_deformed_structures, plotStructureWithMomentDiagram, plotStructureWithShearDiagram, plotStructureWithAxialDiagram
+from graph import plot_original_structure_all_forces, plot_deformed_structures, plotStructureWithMomentDiagram, plotStructureWithShearDiagram, plotStructureWithAxialDiagram
 import matplotlib.pyplot as plt
 
 
@@ -24,21 +24,11 @@ Solver(structure_2.nodes, structure_2.elements, M.kff_matrix, M.kfc_matrix, M.kc
 M = Assembly(structure_3.nodes, structure_3.elements)
 Solver(structure_3.nodes, structure_3.elements, M.kff_matrix, M.kfc_matrix, M.kcf_matrix, M.kcc_matrix)
 
-#plot_original_structure_all_forces(structure_1.nodes, structure_1.elements)
-#plot_original_structure_all_forces(structure_2.nodes, structure_2.elements)
-#plot_original_structure_all_forces(structure_3.nodes, structure_3.elements)
+#plot_deformed_structures([structure_1, structure_2, structure_3], text=False, nodes=True, nodes_labels=False, deformada=True, Escala=[750, 100, 100])
 
-#plot_deformed_structure(structure_1.elements, text=False, nodes=True, nodes_labels=False, deformada=True, escala=4000)
-#plot_deformed_structure(structure_2.elements, text=False, nodes=True, nodes_labels=False, deformada=True, escala=2000)
-#plot_deformed_structure(structure_3.elements, text=False, nodes=True, nodes_labels=False, deformada=True, escala=2000)
-
-#plot_original_structure_all_forces(structure_1.nodes, structure_1.elements)
-
-plot_deformed_structures([structure_1, structure_2, structure_3], text=False, nodes=True, nodes_labels=False, deformada=True, escala=500)
-
-plotStructureWithMomentDiagram(structure_1.elements, escala_momentos=1e-5, show_structure=True)
-plotStructureWithShearDiagram(structure_1.elements, escala_corte=1e-5, show_structure=True)
-plotStructureWithAxialDiagram(structure_1.elements, escala_axial=1e-6, show_structure=True)
+plotStructureWithMomentDiagram([structure_1, structure_2, structure_3], Escala=[1e-5, 1e-6, 1e-6], show_structure=True)
+#plotStructureWithShearDiagram([structure_1, structure_2, structure_3], Escala=[1e-5, 1e-6, 1e-6], show_structure=True)
+#plotStructureWithAxialDiagram([structure_1, structure_2, structure_3], Escala=[1e-6, 1e-6, 1e-6], show_structure=True)
 
 #---------------------------------
 #Con offsets
@@ -46,7 +36,6 @@ plotStructureWithAxialDiagram(structure_1.elements, escala_axial=1e-6, show_stru
 structure_1 = Structure(nombre='Caso A, con offsets rigidos', dxdy = [0.5, 0.0], caso='a')
 structure_2 = Structure(nombre='Caso B, con offsets rigidos', dxdy = [0.5, 0.0], caso='b')
 structure_3 = Structure(nombre='Caso C, con offsets rigidos', dxdy = [0.5, 0.0], caso='c')
-
 
 M = Assembly(structure_1.nodes, structure_1.elements)
 Solver(structure_1.nodes, structure_1.elements, M.kff_matrix, M.kfc_matrix, M.kcf_matrix, M.kcc_matrix)
@@ -57,19 +46,9 @@ Solver(structure_2.nodes, structure_2.elements, M.kff_matrix, M.kfc_matrix, M.kc
 M = Assembly(structure_3.nodes, structure_3.elements)
 Solver(structure_3.nodes, structure_3.elements, M.kff_matrix, M.kfc_matrix, M.kcf_matrix, M.kcc_matrix)
 
-plot_original_structure_all_forces(structure_1.nodes, structure_1.elements)
-plot_original_structure_all_forces(structure_2.nodes, structure_2.elements)
-plot_original_structure_all_forces(structure_3.nodes, structure_3.elements)
+plot_deformed_structures([structure_1, structure_2, structure_3], text=False, nodes=True, nodes_labels=False, deformada=True, Escala=[750, 500, 500])
 
-#plot_deformed_structure(structure_1.elements, text=False, nodes=True, nodes_labels=False, deformada=True, escala=4000)
-#plot_deformed_structure(structure_2.elements, text=False, nodes=True, nodes_labels=False, deformada=True, escala=2000)
-#plot_deformed_structure(structure_3.elements, text=False, nodes=True, nodes_labels=False, deformada=True, escala=2000)
-
-#plot_original_structure_all_forces(structure_1.nodes, structure_1.elements)
-
-plot_deformed_structures([structure_1, structure_2, structure_3], text=False, nodes=True, nodes_labels=False, deformada=True, escala=500)
-
-plotStructureWithMomentDiagram(structure_1.elements, escala_momentos=1e-5, show_structure=True)
-plotStructureWithShearDiagram(structure_1.elements, escala_corte=1e-5, show_structure=True)
-plotStructureWithAxialDiagram(structure_1.elements, escala_axial=1e-6, show_structure=True)
+plotStructureWithMomentDiagram([structure_1, structure_2, structure_3], Escala=[1e-6, 1e-6, 1e-6], show_structure=True)
+#plotStructureWithShearDiagram([structure_1, structure_2, structure_3], Escala=[1e-5, 1e-6, 1e-6], show_structure=True)
+#plotStructureWithAxialDiagram([structure_1, structure_2, structure_3], Escala=[1e-6, 1e-6, 1e-6], show_structure=True)
 
